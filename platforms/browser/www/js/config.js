81da294643aa06f755b72f9d932ce1d230d0b8ab
Sherwin Jupiter <jupiter@pivotmobileapp.com>
2016-12-18 12:17:42 -0400

    fix color

--- platforms/browser/www/js/config.js
+++ platforms/browser/www/js/config.js
@@ -1,5 +1,5 @@
 var krms_config ={			
-	'ApiUrl' : "http://appy-tite.com/mobileapp/api",	
+	'ApiUrl' : "http://appy-tite.com/merchantapp/api",	
 	'DialogDefaultTitle' : "Appy-tite Order taking",
 	'pushNotificationSenderid' : "733140350278",
 	'APIHasKey' : "fed7b441b349bae8f146711fbd215e90"
