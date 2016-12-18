66114ed8e30fc1d2fc8b4928074c4e2beb2f38a0
Sherwin Jupiter <jupiter@pivotmobileapp.com>
2016-12-18 11:07:31 -0400

    first

---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={			
+	'ApiUrl' : "http://appy-tite.com/mobileapp/api",	
+	'DialogDefaultTitle' : "Appy-tite Order taking",
+	'pushNotificationSenderid' : "733140350278",
+	'APIHasKey' : "fed7b441b349bae8f146711fbd215e90"
+};
---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+var krms_config ={			
+	'ApiUrl' : "http://appy-tite.com/merchantapp/api",	
+	'DialogDefaultTitle' : "Appy-tite Order taking",
+	'pushNotificationSenderid' : "733140350278",
+	'APIHasKey' : "fed7b441b349bae8f146711fbd215e90"
+};
